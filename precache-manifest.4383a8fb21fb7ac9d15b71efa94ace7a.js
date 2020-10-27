a0087711adfd8555c8170b1a941b6d83a95af7cc
BossBele <adambeleko@gmail.com>
2020-10-27 14:45:24 +0300

    Updates

---
+++ precache-manifest.4383a8fb21fb7ac9d15b71efa94ace7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "831597a2585cb3da0bcdc130f47279ce",
+    "url": "/react-cloner/index.html"
+  },
+  {
+    "revision": "15f9e687b2095d9c61f1",
+    "url": "/react-cloner/static/css/2.c72968ad.chunk.css"
+  },
+  {
+    "revision": "2772a7fff5833319949f",
+    "url": "/react-cloner/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "15f9e687b2095d9c61f1",
+    "url": "/react-cloner/static/js/2.aaa2eb69.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-cloner/static/js/2.aaa2eb69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2772a7fff5833319949f",
+    "url": "/react-cloner/static/js/main.ee55b1fd.chunk.js"
+  },
+  {
+    "revision": "11986ac659a213d23265",
+    "url": "/react-cloner/static/js/runtime-main.603fbd9b.js"
+  }
+]);
